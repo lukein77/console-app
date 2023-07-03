98a30ab7dc0e294aacb34596138c77a497c6fb64
lukein77 <lukein77@gmail.com>
2023-07-03 12:36:41 -0300

    Update list of commands

--- utils.rb
+++ utils.rb
@@ -10,11 +10,15 @@ def get_command_list
   create_file <name> <content?>   - create a file with optional content
   create_folder <name>            - create a folder
   destroy <name>                  - delete a file or folder
-  rename <name> <new name>        - rename a file or folder
-  cd <folder>                     - enter a directory
+  cd <directory>                  - enter a directory
   ls                              - show the content of the current directory
+  ls <directory>                  - show the content of the specified directory
   metadata <name>                 - show the metadata of a file or folder
   whereami                        - show the full path of the current directory
+  create_user <username>          - register a new user
+  login <username>                - log in as an existing user
+  logout                          - log out of the system
+  whoami                          - show current session data
   exit                            - exit the application
   "
 end
